2e95d29df9a5aa456b5fbc825190b5bd12ecff83
Sandra <88970288+s-and-ra@users.noreply.github.com>
2021-10-18 21:21:30 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar bg="NavbarColor" variant="dark"
+        sticky="top" expand="sm" collapseOnSelect>
+        <Navbar.Brand>
+          Upnature
+        </Navbar.Brand>
+
+        <Navbar.Toggle className="coloring" />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <NavDropdown title="AboutUs">
+              <NavDropdown.Item href="#TheProject">The project</NavDropdown.Item>
+              <NavDropdown.Item href="#Team">Team</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#contactus">Contact Us</Nav.Link>
+            <Nav.link href="#actnow">Start now!</Nav.link>
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar>
+      <div className="content">
+        Here comes the content
+      </div>
     </div>
   );
 }
